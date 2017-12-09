b033601488a4eccd28ddb7a8e5cecc9ee2148a39
Raafje16 <34309777+Raafje16@users.noreply.github.com>
2017-12-09 21:58:49 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -30,22 +30,22 @@ def potMaker(n1, a1, n2, a2, gridSize):
 def task5():
     plotEigenstate(potMaker(2, 10 ** -4, 0, 0, 101)[0], potMaker(2, 10 ** -4, 0, 0, 101)[1], 1, 'Task 5.0')
     for i in np.arange(0, 10 ** -5, 10 ** -6):
-        hulpje = potMaker(2, 10 ** -4, 4, i, 101)  # Slechte naam voor een variabele
-        # plotEigenstate(hulpje[0],hulpje[1],hulpje[2])
+        HVMatrix = potMaker(2, 10 ** -4, 4, i, 101)  
+        # plotEigenstate(HVMatrix[0], HVMatrix[1], HVMatrix[2])
 
         if i == 0:  # Aangezien deze statement altijd True geeft bij de eerste stap in je for kan je wat erin staat
             # misschien beter voor je for loop uitvoeren
-            aMatrix = hulpje[0]
+            energieMatrix = HVMatrix[0]
         if i != 0:
-            aiMatrix = hulpje[0] - aMatrix
+            verschilMatrix = HVMatrix[0] - energieMatrix
             plt.figure('Task 5.2')
-            plt.plot(aiMatrix, 'o', label="a = " + str(i), markersize=2.5)
+            plt.plot(verschilMatrix, 'o', label="a = " + str(i), markersize=2.5)
             plt.legend()
             plt.xlabel('n')
             plt.ylabel('$\Delta$E (A.U)')
             plt.title('Energieverschillen van deltapotential(x) = ax\u2074 + x\u00B2 en deltapotential(x) = x\u00B2')
         plt.figure('Task 5.1')
-        plt.plot(hulpje[0], 'o', label="a = " + str(i), markersize=2.5)
+        plt.plot(HVMatrix[0], 'o', label="a = " + str(i), markersize=2.5)
         plt.legend()
         plt.xlabel('n')
         plt.ylabel('E (A.U)')
