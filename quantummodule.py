6534a3143673179d7b2391800d0c4c874161971e
bb-bb <b.jansen5@students.uu.nl>
2017-12-19 18:04:57 +0100

    add previous next button and some documentation

--- quantummodule.py
+++ quantummodule.py
@@ -11,89 +11,113 @@ g_AxColor = "lightgoldenrodyellow"
 
 
 def getHamiltonMatrix(gridSize, t):
-    """
-    :param gridSize: The size of the Hamiltonmatrix you wish to construct.
-    :param t: The unit energy in hbar/8ma
-    :return: A numpy array of size (gridSize,gridSize) representing the Hamiltonmatrix
-    """
-    return np.diag(-t * np.ones(gridSize - 1), k=-1) \
-           + np.diag(2 * t * np.ones(gridSize)) \
-           + np.diag(-t * np.ones(gridSize - 1), k=1)
+	"""
+	:param gridSize: The size of the Hamiltonmatrix you wish to construct.
+	:param t: The unit energy in hbar/8ma
+	:return: A numpy array of size (gridSize,gridSize) representing the Hamiltonmatrix
+	"""
+	return np.diag(-t * np.ones(gridSize - 1), k=-1) \
+		   + np.diag(2 * t * np.ones(gridSize)) \
+		   + np.diag(-t * np.ones(gridSize - 1), k=1)
 
 
 def calculateEigenstate(hamiltonMatrix, boxSize):
-    """
-    A function that calculates the eigenvalues and eigenvectors of a matrix.
+	"""
+	A function that calculates the eigenvalues and eigenvectors of a matrix.
 
-    :param hamiltonMatrix: A numpy array representing the hamilton matrix for which you wish to know the eigenvalues.
-    :param gridSize: Eigenlijk hoeft dit niet als parameter meegegeven want kan je berekenen uit hamiltonMatrix.
-    :param boxSize: Length of the box
-    :return: A size (2,) tuple with the first element eigenValues and second element normalized eigenVectors.
-    """
-    eigenValues, eigenVectors = np.linalg.eigh(hamiltonMatrix)
-    gridSize = len(eigenValues)
+	:param hamiltonMatrix: A numpy array representing the hamilton matrix for which you wish to know the eigenvalues.
+	:param gridSize: Eigenlijk hoeft dit niet als parameter meegegeven want kan je berekenen uit hamiltonMatrix.
+	:param boxSize: Length of the box
+	:return: A size (2,) tuple with the first element eigenValues and second element normalized eigenVectors.
+	"""
+	eigenValues, eigenVectors = np.linalg.eigh(hamiltonMatrix)
+	gridSize = len(eigenValues)
 
-    # Normalization procedure
-    for i in range(gridSize):
-        eigenVectors[:, i] /= np.sqrt(boxSize * np.sum(eigenVectors[:, i] ** 2 / gridSize))
+	# Normalization procedure
+	for i in range(gridSize):
+		eigenVectors[:, i] /= np.sqrt(boxSize * np.sum(eigenVectors[:, i] ** 2 / gridSize))
 
-    return eigenValues, eigenVectors
+	return eigenValues, eigenVectors
 
 
 def plotEigenstate(eigenValues, eigenVectors, boxSize, windowtitle):
-    gridSize = len(eigenValues)
+	"""
+	A function plotting the eigenValues, an eigenStates and eigenState squared,
+	with some slider to scroll through the eigenStates and eigenState squared.
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    fig.canvas.set_window_title(windowtitle)
-    ax1.plot(eigenValues, "o", markersize=3)
-    ax1.set_title("Energy")
-    ax1.set_ylabel("E (a.u.)")
-    ax1.set_xlabel("n")
+	:param eigenValues: Sorted set of eigenvalues
+	:eigenVectors: eigenvectors in the order to their corresponding eigenvalues
+	:param boxSize: Length of the box
+	"""
+	gridSize = len(eigenValues)
 
-    n0 = 0
+	fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+	fig.canvas.set_window_title(windowtitle)
+	ax1.plot(eigenValues, "o", markersize=3)
+	ax1.set_title("Energy")
+	ax1.set_ylabel("E (a.u.)")
+	ax1.set_xlabel("n")
 
-    eigenVector = eigenVectors[:, n0]
-    l, = ax2.plot(np.linspace(0, boxSize, num=gridSize), eigenVector ** 2)
-    ax3.set_ylim([0, 1.1 * np.max(eigenVectors) ** 2])
-    ax2.set_ylabel(r"$|\psi|^2$")
-    ax2.set_xlabel("x (A.U)")
-    ax2.set_title("Probability density")
+	n0 = 0
 
-    l2, = ax3.plot(np.linspace(0, boxSize, num=gridSize), eigenVector)
-    ax3.set_ylim([1.1 * np.min(eigenVectors), 1.1 * np.max(eigenVectors)])
-    ax3.set_ylabel(r"$\psi$")
-    ax3.set_xlabel("x (A.U)")
-    ax3.set_title("Wave function")
+	eigenVector = eigenVectors[:, n0]
+	l, = ax2.plot(np.linspace(0, boxSize, num=gridSize), eigenVector ** 2)
+	ax3.set_ylim([0, 1.1 * np.max(eigenVectors) ** 2])
+	ax2.set_ylabel(r"$|\psi|^2$")
+	ax2.set_xlabel("x (a.u)")
+	ax2.set_title("Probability density")
 
-    plt.tight_layout()
+	l2, = ax3.plot(np.linspace(0, boxSize, num=gridSize), eigenVector)
+	ax3.set_ylim([1.1 * np.min(eigenVectors), 1.1 * np.max(eigenVectors)])
+	ax3.set_ylabel(r"$\psi$")
+	ax3.set_xlabel("x (a.u)")
+	ax3.set_title("Wave function")
 
-    sliderAx = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=g_AxColor)
-    slider = Slider(sliderAx, "n", 0, gridSize - 1, valinit=n0, valfmt="%d")
+	plt.tight_layout()
 
-    def update(val):
-        n = int(slider.val)
-        y = eigenVectors[:, n]
-        l.set_ydata(y ** 2)
-        l2.set_ydata(y)
-        ax2.set_title("Probability distribution for eigenvalue = {:4.3f}".format(eigenValues[n]))
-        fig.canvas.draw_idle()
+	sliderAx = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=g_AxColor)
+	slider = Slider(sliderAx, "n", 0, gridSize - 1, valinit=n0, valfmt="%d")
 
-    slider.on_changed(update)
+	def update(val):
+		n = int(slider.val)
+		y = eigenVectors[:, n]
+		l.set_ydata(y ** 2)
+		l2.set_ydata(y)
+		ax2.set_title("Probability distribution for eigenvalue = {:4.3f}".format(eigenValues[n]))
+		fig.canvas.draw_idle()
 
-    resetAx = plt.axes([0.75, 0.025, 0.1, 0.04])
-    resetButton = Button(resetAx, "Reset", color=g_AxColor, hovercolor="0.975")
+	slider.on_changed(update)
 
-    def reset(event):
-        slider.reset()
+	plt.subplots_adjust(bottom=0.25)
 
-    resetButton.on_clicked(reset)
+	closeAx = plt.axes([0.85, 0.025, 0.1, 0.04])
+	closeButton = Button(closeAx, "Close", color=g_AxColor, hovercolor="0.975")
 
-    plt.subplots_adjust(bottom=0.25)
+	def close(event):
+		plt.close()
 
-    closeAx = plt.axes([0.85, 0.025, 0.1, 0.04])
-    closeButton = Button(closeAx, "Close", color=g_AxColor, hovercolor="0.975")
+	resetAx = plt.axes([0.65, 0.025, 0.1, 0.04])
+	resetButton = Button(resetAx, "Reset", color=g_AxColor, hovercolor="0.975")
 
-    def close(event):
-        plt.close()
+	def reset(event):
+		slider.reset()
 
-    closeButton.on_clicked(close)
+	resetButton.on_clicked(reset)
+
+	closeButton.on_clicked(close)
+
+	nextAx = plt.axes([0.55, 0.025, 0.1, 0.04])
+	nextButton = Button(nextAx, "Next", color=g_AxColor, hovercolor="0.975")
+
+	def next(event):
+		slider.set_val((slider.val + 1) % gridSize)
+
+	nextButton.on_clicked(next)
+
+	prevAx = plt.axes([0.45, 0.025, 0.1, 0.04])
+	prevButton = Button(prevAx, "Previous", color=g_AxColor, hovercolor="0.975")
+
+	def previous(event):
+		slider.set_val((slider.val - 1) % gridSize)
+
+	prevButton.on_clicked(previous)
